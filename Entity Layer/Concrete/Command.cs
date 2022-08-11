9a6d71d934eb1db07ae4f25d83ed06fffe4c8b74
Ayşegül Yılmaz <83874900+aysegulyilmaz5@users.noreply.github.com>
2022-08-11 23:29:02 +0300

    Entity Layer

---
+++ Entity Layer/Concrete/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer.Concrete
+{
+    public class Command
+    {
+        [Key]
+        public int CommandID { get; set; }
+
+        public string CommandUsername { get; set; }
+
+        public string CommandTitle { get; set; }
+
+        public string CommandContent { get; set; }
+
+        public DateTime CommandDate { get; set; }
+
+        public int BlogScore { get; set; }
+
+        public bool CommandStatus { get; set; }
+
+        public int BlogID { get; set; }
+
+        public Blog Blog { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
---
+++ Entity Layer/Concrete/MyData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Model
+{
+    public class MyData
+    {
+        public string FieldOne { get; set; }
+    }
+}
